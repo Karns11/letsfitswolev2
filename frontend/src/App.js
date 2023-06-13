73cf3eee1a15b54d03c596d75b9c3c1e6bc11e11
nathan karns <Nathan k>
2023-06-13 18:22:28 -0400

    new approach with mainscreen and profile screen included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import MainScreen from "./screens/MainScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const PrivateRoute = ({ element: Element, ...rest }) => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return userInfo ? <Element /> : <Navigate to="/login" replace />;
+};
 
 function App() {
   return (
@@ -12,6 +21,14 @@ function App() {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route
+          path="/mainscreen"
+          element={<PrivateRoute element={MainScreen} />}
+        />
+        <Route
+          path="/profile"
+          element={<PrivateRoute element={ProfileScreen} />}
+        />
       </Routes>
       <ToastContainer />
     </BrowserRouter>

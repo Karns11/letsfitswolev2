6b4e291f6282b5b5cc62af009bdf366f28c1b78a
nathan karns <Nathan k>
2023-06-13 18:07:56 -0400

    try diff approach

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
-    <div>
-      <Outlet />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+      </Routes>
       <ToastContainer />
-    </div>
+    </BrowserRouter>
   );
 }
 

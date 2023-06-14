5653c73ca6c09d59fcf6e0af4a3ab2bf05929475
nathan karns <Nathan k>
2023-06-13 20:58:59 -0400

    rollback to react-router v 6.11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useSelector } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import MainScreen from "./screens/MainScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import WorkoutScreen from "./screens/WorkoutScreen";
-import AddFriendsScreen from "./screens/AddFriendsScreen";
-import FriendScreen from "./screens/FriendScreen";
-
-const PrivateRoute = ({ element: Element, ...rest }) => {
-  const { userInfo } = useSelector((state) => state.auth);
-
-  return userInfo ? <Element /> : <Navigate to="/login" replace />;
-};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-      </Routes>
+    <div>
+      <Outlet />
       <ToastContainer />
-    </BrowserRouter>
+    </div>
   );
 }
 
